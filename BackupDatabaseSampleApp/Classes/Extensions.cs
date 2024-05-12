ca3a354d73420ec5a9788ee5a00a1009deaf2590
Karen Payne <paynekaren@comcast.net>
2024-05-12 10:37:29 -0700

    back-up database sample, strange issue with git.

---
+++ BackupDatabaseSampleApp/Classes/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SQLite;
+
+namespace BackupDatabaseSampleApp.Classes;
+
+internal static class Extensions
+{
+    public static void BackupDatabase(this SQLiteConnection source, SQLiteConnection destination)
+    {
+        source.Open();
+        destination.Open();
+        source.BackupDatabase(destination, "main", "main", -1, null, 0);
+    }
+}
---
+++ BackupDatabaseSampleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SQLite;
+using BackupDatabaseSampleApp.Classes;
+
+namespace BackupDatabaseSampleApp;
+
+internal partial class Program
+{
+    private static string _name = "NorthWindContacts.db";
+    private static string ConnectionString()
+        => $"Data Source={_name}";
+
+    private static string _backupDatabaseName => "BackupSample.db";
+
+
+    static void Main(string[] args)
+    {
+
+        AnsiConsole.MarkupLine("[cyan]Backing up database[/]");
+        try
+        {
+            if (File.Exists(_backupDatabaseName))
+            {
+                File.Delete(_backupDatabaseName);
+            }
+
+            var backup = new SQLiteConnection($"Data Source={_backupDatabaseName}");
+            using var cn = new SQLiteConnection(ConnectionString());
+            cn.BackupDatabase(backup);
+            AnsiConsole.MarkupLine("[green]Database backed up successfully[/]");
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.WriteException(ex);
+        }
+
+        Console.ReadLine();
+    }
+    
+}
