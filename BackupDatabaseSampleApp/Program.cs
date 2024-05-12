440a42eb12bd063bf11e5c8436244d5ecee16b8e
Karen Payne <paynekaren@comcast.net>
2024-05-12 15:54:20 -0700

    :heavy_check_mark: backup code

--- BackupDatabaseSampleApp/Program.cs
+++ BackupDatabaseSampleApp/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System.Data.SQLite;
-using System.IO;
-using System.Text.Json;
 using BackupDatabaseSampleApp.Classes;
-using ConsoleConfigurationLibrary.Classes;
-using Microsoft.Extensions.Configuration;
+using BackupDatabaseSampleApp.Models;
+using Dapper;
 
 namespace BackupDatabaseSampleApp;
 
@@ -11,48 +9,52 @@ internal partial class Program
 {
     private static string _name = "NorthWindContacts.db";
     private static string ConnectionString() => $"Data Source={_name}";
-
     
-
-
     static void Main(string[] args)
     {
 
-        var _configuration = Configuration.JsonRoot();
-        var appSettings = _configuration.GetRequiredSection("Settings").Get<AppSettings>();
-        AnsiConsole.MarkupLine("[yellow]ServiceDetails[/]");
-        Console.WriteLine($"   UseMemory: {appSettings.UseMemory}");
-        Console.WriteLine($"Cors.Origins: {appSettings.Cors.Origins}");
-        Console.WriteLine($"Cors.Headers: {appSettings.Cors.Headers}");
-        Console.WriteLine($"Cors.Methods: {appSettings.Cors.Methods}");
+        var list = Contacts();
 
-        //BackupOperations.PerformBackup(
-        //    Path.Combine("Backups", Path.GetFileName(GenerateFiles.NextFileName())!), 
-        //    ConnectionString());
+        var table = CreateTable();
+        foreach (var contact in list)
+        {
+            table.AddRow(contact.Id.ToString(), contact.FirstName, contact.LastName);
+        }
+        
+        AnsiConsole.Write(table);
 
-        Console.ReadLine();
+        Console.WriteLine();
+        
+        var (success, exception) = BackupOperations.PerformBackup(Path.Combine("Backups", Path.GetFileName(GenerateFiles.NextFileName())!));
+        if (success)
+        {
+            AnsiConsole.MarkupLine("[LightGreen]Backup completed successfully![/]");
+        }
+        else
+        {
+            AnsiConsole.WriteException(exception);
+        }
+
+        SpectreConsoleHelpers.ExitPrompt();
     }
 
-}
+    public static List<Contact> Contacts()
+    {
+        using var cn = new SQLiteConnection(BackupSettings.Instance.ConnectionString);
+        return cn.Query<Contact>(SqlStatements.GetContactsWithOfficePhone).ToList();
+    }
 
-public class AppSettings
-{
-    public bool? UseMemory { get; set; }
-    public CorsSettings? Cors { get; set; }
-}
+    public static Table CreateTable()
+    {
+        var table = new Table()
+            .RoundedBorder()
+            .AddColumn("[b]Id[/]")
+            .AddColumn("[b]First[/]")
+            .AddColumn("[b]Last[/]")
+            .Alignment(Justify.Left)
+            .BorderColor(Color.LightSlateGrey)
+            .Title("[LightGreen]Contacts top 15[/]");
+        return table;
+    }
 
-public class CorsSettings
-{
-    public required string Origins { get; set; }
-    public required string Headers { get; set; }
-    public required string Methods { get; set; }
-}
-public class Settings1
-{
-    public MailAddress[] MailAddress { get; set; }
-}
-public class MailAddress
-{
-    public string Address { get; set; }
-    public string Display { get; set; }
 }
--- BackupDatabaseSampleApp/readme.md
+++ BackupDatabaseSampleApp/readme.md
@@ -1,1 +1,26 @@
 ﻿# About
+
+An example to show how to backup and SQLite database.
+
+On first build a folder named Backups is creating from the following in this project file.
+
+```xml
+<Target Name="MakeTempFolder" AfterTargets="Build">
+   <MakeDir Directories="$(OutDir)Backups" />
+</Target>
+```
+
+Next top 15 contacts are presented followed by backing up the database.
+
+- `AppSettings` is a container for values read from appsettings.json
+    - BaseFileName for backups
+    - BaseExtension of BaseFileName
+    - ConnectionString, connection string for main datase
+- `BackupOperations` is where the database backup is performed
+- `BackupSettings` is a Singleton to provide access to `AppSettings` values
+- `GenerateFiles` is responsible for generating file names for backup operations
+
+## Generate file names
+
+Its important to understand why the base file name and base extension are dynamic, read from appsettings.json is so the class can be used in other projects.
+
